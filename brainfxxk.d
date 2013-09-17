7e02c16856facad918c82afcd4d6c92e68a74da7
Hiroki Noda <kubo39@gmail.com>
2013-09-17 12:01:46 +0900

    initial commit

---
+++ brainfxxk.d
@@ -0,0 +1,61 @@
+import std.stdio, std.file;
+
+uint MAX_PROG_LEN = 30000;
+
+void main(string[] args) {
+    auto program = cast(string) read(args[1], MAX_PROG_LEN);
+
+    // prepare VM
+    ubyte[3000] data;   // hmm,,,
+    uint data_ptr = 0;
+    uint loop_depth = 0;
+    
+    // execution loop
+    for (int instruction_ptr; instruction_ptr < program.length; ++instruction_ptr) {
+	switch (program[instruction_ptr]) {
+	case '>': data_ptr++; break;
+	case '<': data_ptr--; break;
+	case '+': data[data_ptr]++; break;
+	case '-': data[data_ptr]--; break;
+	case '.': {
+	    write(cast(char) data[data_ptr]);
+	    break;
+	}
+	case ',': {  // looks ugly...
+	    char[] buf;
+	    stdin.readln(buf);
+	    data[data_ptr] = cast(ubyte) buf[0];
+	    break;
+	}
+	case '[': {
+	    if (data[data_ptr] == 0) {
+		instruction_ptr++;
+		while (loop_depth > 0 || program[instruction_ptr] != ']') {
+		    if (program[instruction_ptr] == '[') {
+			loop_depth++;
+		    } else if (program[instruction_ptr] == ']') {
+			loop_depth--;
+		    }
+		    instruction_ptr++;
+		} 
+	    }
+	    break;
+	}
+	case ']': {
+	    instruction_ptr--;
+	    while (loop_depth >0 || program[instruction_ptr] != '[') {
+		if (program[instruction_ptr] == ']') {
+		    loop_depth++;
+		} else if (program[instruction_ptr] == '[') {
+		    loop_depth--;
+		}
+		instruction_ptr--;
+	    }
+	    instruction_ptr--;
+	    break;
+	}
+	default: break;
+	}
+    }
+    writeln();
+}
